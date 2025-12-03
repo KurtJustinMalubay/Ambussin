9c70d4382d8a0e9a949b8fe2c78cb56d23d1decb
Joel Theo Gallarde <gallardejoeltheo@gmail.com>
2025-12-03 18:58:08 +0800

    I remade the entire GUI, added more stuff. redesigned the whole entire thing. ang kuwang nalang guro is ang pag connect ninyo ani sa inyong back end. also about sa registration maybe mag himo nalang kog para sa registration pud unya puhon

--- src/main/BusBookingApp.java
+++ src/main/BusBookingApp.java
@@ -658,6 +658,7 @@ public class BusBookingApp extends JFrame {
         p.add(new JLabel(text));
         return p;
     }
+    //connect
     //help
 
     // --- CUSTOM BUTTON CLASS FOR MODERN LOOK ---
