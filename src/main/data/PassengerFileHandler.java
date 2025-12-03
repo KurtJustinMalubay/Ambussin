ae8a4f8a29704425daf2a3c246b1b153494508b6
Wilmark Racho <s.wilmarkracho@gmail.com>
2025-12-03 20:42:35 +0800

    Ignore passenger.txt

---
+++ src/main/data/PassengerFileHandler.java
@@ -0,0 +1,43 @@
+package data;
+
+import main.models.Passenger;
+import java.io.*;
+import java.util.*;
+
+public class PassengerFileHandler {
+    private static final String FILE_PATH = "passengers.txt";
+
+    public static void savePassenger(Passenger passenger) {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH, true))){
+            bw.write(passenger.getName() + "," + passenger.getAge() + "," + passenger.getPassengerType());
+            bw.newLine();
+        }catch(IOException e){
+            System.out.println("Error writing passenger:  " + e.getMessage());
+        }
+    }
+    public static List<Passenger> loadPassengers() {
+        List<Passenger> passengers = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                if (data.length == 3) {
+                    String name = data[0];
+                    int age = Integer.parseInt(data[1]);
+                    String type = data[2];
+
+                    Passenger p = new Passenger(name, age, type);
+                    passengers.add(p);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading passengers: " + e.getMessage());
+        }
+
+        return passengers;
+    }
+}
