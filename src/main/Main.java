e88e75c68467238ced5b73c15c1d598f4b09464a
Joel Theo Gallarde <gallardejoeltheo@gmail.com>
2025-12-03 18:51:44 +0800

    I remade the entire GUI, added more stuff. redesigned the whole entire thing. ang kuwang nalang guro is ang pag connect ninyo ani sa inyong back end. also about sa registration maybe mag himo nalang kog para sa registration pud unya puhon

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ import com.ambussin.BusBookingApp;  // Make sure to import the GUI class
 public class Main {
 
     public static void main(String[] args) {
-        // This is the code that launches the GUI window.
+
         SwingUtilities.invokeLater(BusBookingApp::new);
     }
 }
